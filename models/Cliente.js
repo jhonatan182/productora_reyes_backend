a73fed5fa91f06da6b06dd2e3225e4250158d1ce
jhonatan182 <jona.vand98@gmail.com>
2022-10-17 10:02:30 -0600

    modelos, controllers, y routes para para clientes

--- models/Cliente.js
+++ models/Cliente.js
@@ -1,16 +1,47 @@
 import { DataTypes } from 'sequelize';
 import db from '../config/db.js';
 
-const Cliente = db.define('cliente', {
-    id_cliente: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
+const Cliente = db.define(
+    'cliente',
+    {
+        id_cliente: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nombre_cliente: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        apellido_cliente: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        identidad_cliente: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        edad: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        telefono_cliente: {
+            type: DataTypes.STRING(45),
+            allowNull: false,
+        },
+        correo_cliente: {
+            type: DataTypes.STRING(90),
+            allowNull: false,
+        },
+        direccion_cliente: {
+            type: DataTypes.STRING(120),
+            allowNull: false,
+        },
     },
-    nombre_cliente: {
-        type: DataTypes.STRING(45),
-        allowNull: false,
-    },
-});
+    {
+        tableName: 'cliente',
+        timestamps: false,
+    }
+);
 
 export default Cliente;
