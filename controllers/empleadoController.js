4d4f887779cd1ac538e81f7bd7641cae10f2cf42
moises199802 <moisesdavid201@gmail.com>
2022-11-12 17:00:51 -0600

    Empleados con Json y quitando checks

--- controllers/empleadoController.js
+++ controllers/empleadoController.js
@@ -15,7 +15,7 @@ export const listarEmpleados = async (req, res) => {
             return res.status(404).json({ msg: 'No hay empleados' });
         }
         else {
-        return res.status(200).json({msg: 'Lista de empleados', empleados});
+            return res.status(200).json(empleados);
         }
     } catch (error) {
         console.log(error);
@@ -49,14 +49,14 @@ export const crearEmpleados = async (req, res) => {
            
         });
         await usuario.save();
-        res.status(200).json({ msg: 'Empleado creado correctamente', empleado});
+        res.status(200).json({ msg: 'Empleado creado correctamente' });
       
     } catch (error) {
        // console.log(error);
         res.status(500).send('Hubo un error ' +error.message);
     }
 };
-
+//actualizar por id
 export const actualizarEmpleado = async (req, res) => {
     if (req.usuario.descripcion_rol !== 'admin') {
         const error = new Error('No tiene permiso');
@@ -68,67 +68,64 @@ export const actualizarEmpleado = async (req, res) => {
         return res.status(400).json({ errores: errores.array() });
     }
 
-    //? extraer la informacion del empleado
-    const { nombre_empleado, apellido_empleado,identidad_empleado, telefono_empleado, direccion_empleado, correo_empleado,rol_id ,} = req.body;
-    const nuevoEmpleado = {};
-    if (nombre_empleado) {
-        nuevoEmpleado.nombre_empleado = nombre_empleado;
-    }
-    if (apellido_empleado) {
-        nuevoEmpleado.apellido_empleado = apellido_empleado;
-    }
-    if (identidad_empleado) {
-        nuevoEmpleado.identidad_empleado = identidad_empleado;
-    }
-    if (telefono_empleado) {
-        nuevoEmpleado.telefono_empleado = telefono_empleado;
-    }
-    if (direccion_empleado) {
-        nuevoEmpleado.direccion_empleado = direccion_empleado;
-    }
-    if (correo_empleado) {
-        nuevoEmpleado.correo_empleado = correo_empleado;
-    }
-    if (rol_id) {
-        nuevoEmpleado.rol_id = rol_id;
-        
-    }   
+    const { id } = req.params;
     try {
         //? revisar el ID
-        let empleado = await empleadoModel.findByPk(req.query.id_empleado);
+        let empleado = await empleadoModel.findByPk(id);
         //? si el empleado existe o no
         if (!empleado) {
             return res.status(404).json({ msg: 'Empleado no encontrado' });
         }
         //? actualizar
-        empleado = await empleadoModel.update({ ...nuevoEmpleado }, { where: { id_empleado: req.query.id_empleado} });
-        res.status(200).json({ msg: 'Empleado actualizado correctamente',nuevoEmpleado });
+        await empleado.update(req.body);
+        res.status(200).json({ msg: 'Empleado actualizado correctamente' });
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
     }
 };
 
+//eliminar por id       
+
 export const eliminarEmpleado = async (req, res) => {
+    if (req.usuario.descripcion_rol !== 'admin') {
+        const error = new Error('No tiene permiso');
+        return res.status(404).json({ msg: error.message });
+    }
+    const { id } = req.params;
+    try {
+        //? revisar el ID
+        let empleado = await empleadoModel.findByPk(id);
+        //? si el empleado existe o no
+
+        if (!empleado) {
+            return res.status(404).json({ msg: 'Empleado no encontrado' });
+        }
+        //? eliminar el empleado
+        await empleado.destroy();
+        res.status(200).json({ msg: 'Empleado eliminado correctamente' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+};
+//obtener empleado por id
+export const obtenerEmpleado = async (req, res) => {
     if (req.usuario.descripcion_rol !== 'admin') {
         const error = new Error('No tiene permiso');
         return res.status(404).json({ msg: error.message });
     }
     try {
         //? revisar el ID
-        let empleado = await empleadoModel.findByPk(req.query.id_empleado);
+        let empleado = await empleadoModel.findByPk(req.params.id);
         //? si el empleado existe o no
         if (!empleado) {
             return res.status(404).json({ msg: 'Empleado no encontrado' });
         }
-        //? Eliminar el empleado
-        await empleadoModel.destroy({ where: { id_empleado: req.query.id_empleado } });
-        res.status(200).json({ msg: 'Empleado eliminado correctamente' });
+        res.status(200).json(empleado);
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
     }
 }
-
-
 
